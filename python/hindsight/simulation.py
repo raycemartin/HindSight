37b44e7870c455683b2b18afab8d1406981577f8
Rayce Martin <53666496+raycemartin@users.noreply.github.com>
2025-02-04 22:03:07 -0600

    added a little more to simulation.py

--- python/hindsight/simulation.py
+++ python/hindsight/simulation.py
@@ -2,4 +2,10 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-class Image()
+class Image:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        self.img = np.zeros(self.width, self.height)
+
